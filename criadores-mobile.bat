563e95e33b64b7a5983c4f8a45effee93d0f9c08
C0nanT <conantorreswf@gmail.com>
2025-04-08 09:36:18 -0300

    feat: abre editor ao iniciar servidor

--- criadores-mobile.bat
+++ criadores-mobile.bat
@@ -3,4 +3,5 @@
 start "Criadores Mobile Backend" cmd /k "cd C:\apache\htdocs\app-criadores-backend && php artisan serve --host 0.0.0.0"
 
 cd C:\apache\htdocs\app-criadores-frontend
+code .
 npx expo start
